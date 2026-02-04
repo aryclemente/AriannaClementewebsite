fb8d6b3f0cc0f800a8e51fe711408e9948d5dd91
aryclemente <hello.aryc@gmail.com>
2026-02-04 15:29:46 -0400

    feat(build): add Tailwind build pipeline and compiled CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html", "./**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          black: "#141414",
+          gray: "#808080",
+          beige: "#edece7",
+          pink: "#f55481",
+          teal: "#27a397",
+          purple: "#504382",
+          lightPink: "#fbbec6",
+          lightTeal: "#ddfaf8",
+          lavender: "#9fa5c8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
